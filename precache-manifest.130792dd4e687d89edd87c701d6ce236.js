0dd23575ae6e5563dae9311ef98e8332e29814be
olegg789 <olegchikelev789789@gmail.com>
2022-04-10 11:56:08 +0300

    Updates

---
+++ precache-manifest.130792dd4e687d89edd87c701d6ce236.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "bba971bac85af62f3594",
+    "url": "./static/css/main.fbb40c55.chunk.css"
+  },
+  {
+    "revision": "bba971bac85af62f3594",
+    "url": "./static/js/main.9376ec98.chunk.js"
+  },
+  {
+    "revision": "50fc5d0a870517ad7f6b",
+    "url": "./static/js/runtime~main.6bc9254e.js"
+  },
+  {
+    "revision": "d500263ed119b106d0fa",
+    "url": "./static/css/2.1f545151.chunk.css"
+  },
+  {
+    "revision": "d500263ed119b106d0fa",
+    "url": "./static/js/2.7249de96.chunk.js"
+  },
+  {
+    "revision": "f9b4f963298a77e73716",
+    "url": "./static/js/3.a7487db1.chunk.js"
+  },
+  {
+    "revision": "403531465a482bb24f39",
+    "url": "./static/js/4.875eafa4.chunk.js"
+  },
+  {
+    "revision": "cfac030a94dc70214894",
+    "url": "./static/js/5.cafe36ff.chunk.js"
+  },
+  {
+    "revision": "e43e2649a8cf8ea2a1e9",
+    "url": "./static/js/6.3300ad1d.chunk.js"
+  },
+  {
+    "revision": "46b6fea88b21675d08c5",
+    "url": "./static/js/7.e6b1bd3f.chunk.js"
+  },
+  {
+    "revision": "302f790d20dbaa7cec64",
+    "url": "./static/js/8.2ec37693.chunk.js"
+  },
+  {
+    "revision": "ca2729822021e622e24d05c5e27290f9",
+    "url": "./index.html"
+  }
+];
---
+++ static/js/6.3300ad1d.chunk.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{228:function(d,n,a){"use strict";a.r(n);var i=a(223),w=a(224),o=a(225);i.init(),i.add(w),i.add(o),n.default=i}}]);
